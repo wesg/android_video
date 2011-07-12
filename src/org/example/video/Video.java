667f502adcd66ee3acb41a6b9e93eb2127679fbd
Wesley Gorman <rubyist@irubyist.com>
2011-07-12 20:04:36 +0100

    Implemented VideoView to play simple video

--- src/org/example/video/Video.java
+++ src/org/example/video/Video.java
@@ -2,12 +2,20 @@ package org.example.video;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class Video extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Fill view from resource
         setContentView(R.layout.main);
+        VideoView video = (VideoView) findViewById(R.id.video);
+        
+        // Load the video and start the movie
+        video.setVideoPath("/data/samplevideo.3gp");
+        video.start();
     }
 }
