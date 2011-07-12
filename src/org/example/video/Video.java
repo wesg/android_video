64c49b81f8128a03b9a8092b1f59b51f90ab1c2a
Wesley Gorman <rubyist@irubyist.com>
2011-07-12 19:49:00 +0100

    Created android_video project

---
+++ src/org/example/video/Video.java
@@ -0,0 +1,13 @@
+package org.example.video;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Video extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
